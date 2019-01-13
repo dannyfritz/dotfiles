1f912401eea9e7216cec6b10f7fff9e98487dac8
Danny Fritz <dannyfritz@gmail.com>
2019-01-12 18:11:56 -0700

    🎉 2019

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -Tsf $(pwd)/.zshrc ~/.zshrc
+ln -Tsf $(pwd)/.vimrc ~/.vimrc
+ln -Tsf $(pwd)/.tmux.conf ~/.tmux.conf
+ln -Tsf $(pwd)/.editorconfig ~/.editorconfig
+ln -Tsf $(pwd)/.eslintrc ~/.eslintrc
+ln -Tsf $(pwd)/.npmrc ~/.npmrc
+ln -Tsf $(pwd)/.gitconfig ~/.gitconfig
