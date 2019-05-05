a6f32170f5fe203111cab6d42ce1afef947f2a07
Danny Fritz <dannyfritz@gmail.com>
2019-05-05 17:08:49 -0600

    psqlrc

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ ln -Tsf $(pwd)/.zshrc ~/.zshrc
 ln -Tsf $(pwd)/.vimrc ~/.vimrc
 ln -Tsf $(pwd)/.tmux.conf ~/.tmux.conf
 ln -Tsf $(pwd)/.editorconfig ~/.editorconfig
-ln -Tsf $(pwd)/.eslintrc ~/.eslintrc
 ln -Tsf $(pwd)/.npmrc ~/.npmrc
 ln -Tsf $(pwd)/.gitconfig ~/.gitconfig
 ln -Tsf $(pwd)/.gitignore ~/.gitignore
+ln -Tsf $(pwd)/.psqlrc ~/.psqlrc
