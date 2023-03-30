aa9e18f8219a6817be3a22cea8cced37b1a97c71
Danny Fritz <dannyfritz@gmail.com>
2023-03-29 20:32:44 -0500

    rename files. lvim config

---
+++ lvim.lua
@@ -0,0 +1,302 @@
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = false
+lvim.colorscheme = "vscode"
+lvim.lsp.diagnostics.virtual_text = false
+vim.opt.timeoutlen = 100
+vim.opt.mouse = ""
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 2
+vim.opt.wrap = false
+vim.opt.spell = false
+vim.opt.spelllang = "en"
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.g.bookmark_auto_close = 1
+vim.g.bookmark_auto_save = 1
+vim.g.bookmark_save_per_working_dir = 1
+vim.g.bookmark_no_default_key_mappings = 1
+vim.opt.colorcolumn = "80,120"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+-- unmap a default keymapping
+-- vim.keymap.del("n", "<C-Up>")
+-- override a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
+
+-- Change theme settings
+-- lvim.builtin.theme.options.dim_inactive = true
+-- lvim.builtin.theme.options.style = "storm"
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+
+-- Trouble
+-- lvim.builtin.which_key.mappings["t"] = {
+-- "<cmd>Trouble document_diagnostics<cr>", "Diagnostics"
+-- name = "+Trouble",
+-- r = { "<cmd>Trouble lsp_references<cr>", "References" },
+-- f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+-- d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+-- q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+-- l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+-- w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+-- }
+
+-- Hop Mapping
+lvim.builtin.which_key.mappings["H"] = {
+  name = "+Hop",
+  w = { "<cmd>HopWord<cr>", "Word" },
+  p = { "<cmd>HopPattern<cr>", "Pattern" },
+  b = { "<cmd>HopPatternMW<cr>", "Pattern Visible Buffers" },
+  H = { "<cmd>HopPatternMW<cr>", "Pattern Visible Buffers" },
+}
+
+-- Todos Mapping
+lvim.builtin.which_key.mappings["s"]["T"] =
+{
+  "<cmd>TodoTelescope<cr>",
+  "Todos"
+}
+
+-- Spectre Mapping
+lvim.builtin.which_key.mappings["s"]["F"] =
+{
+  "<cmd>Spectre<cr>",
+  "Find & Replace"
+}
+
+-- Bookmarks Mapping
+lvim.builtin.which_key.mappings["s"]["m"] =
+{
+  "<cmd>Telescope vim_bookmarks all<cr>",
+  "Bookmarks"
+}
+
+lvim.builtin.which_key.mappings["m"] = {
+  name = "+Bookmarks",
+  l = { "<cmd>Telescope vim_bookmarks all<cr>", "List" },
+  m = { "<cmd>BookmarkToggle<cr>", "Toggle" },
+  j = { "<cmd>BookmarkNext<cr>", "Next" },
+  k = { "<cmd>BookmarkPrev<cr>", "Previous" },
+  J = { "<cmd>BookmarkMoveUp<cr>", "Move Down" },
+  K = { "<cmd>BookmarkMoveDown<cr>", "Move Up" },
+  a = { "<cmd>BookmarkAnnotate<cr>", "Annotate" },
+  c = { "<cmd>BookmarkClear<cr>", "Clear" },
+  C = { "<cmd>BookmarkClearAll<cr>", "Clear All" },
+}
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = false
+lvim.builtin.breadcrumbs = { active = false }
+lvim.builtin.bufferline = { active = true }
+lvim.builtin.nvimtree.setup.git.ignore = true
+lvim.builtin.nvimtree.setup.renderer.group_empty = true
+lvim.builtin.nvimtree.setup.renderer.highlight_git = true
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.view.side = "right"
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  -- "c",
+  "javascript",
+  "json",
+  "lua",
+  -- "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  -- "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+-- generic LSP settings
+
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+-- lvim.lsp.installer.setup.ensure_installed = {
+--     "sumneko_lua",
+--     "jsonls",
+-- }
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  --   { command = "black", filetypes = { "python" } },
+  --   { command = "isort", filetypes = { "python" } },
+  {
+    command = "prettier",
+  },
+}
+
+-- set additional linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  --   { command = "flake8", filetypes = { "python" } },
+  --   {
+  --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --     command = "shellcheck",
+  --     ---@usage arguments to pass to the formatter
+  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --     extra_args = { "--severity", "warning" },
+  --   },
+  --   {
+  --     command = "codespell",
+  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+  --     filetypes = { "javascript", "python" },
+  --   },
+  {
+    command = "eslint"
+  }
+}
+
+-- Additional Plugins
+lvim.plugins = {
+  { "tpope/vim-repeat" },
+  { "Mofiqul/vscode.nvim" },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    branch = "v2",
+    config = function()
+      require "hop".setup {}
+    end
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require "colorizer".setup({ "css", "javascript", "typesript" }, {
+        css = true,
+        css_fn = true,
+        hsl_fn = true,
+        rgb_fn = true,
+      })
+    end
+  },
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    event = "BufRead",
+    config = function()
+      require "todo-comments".setup {}
+    end
+  },
+  {
+    "itchyny/vim-cursorword",
+    event = { "BufEnter", "BufNewFile" },
+    config = function()
+      vim.api.nvim_command("augroup user_plugin_cursorword")
+      vim.api.nvim_command("autocmd!")
+      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+      vim.api.nvim_command("augroup END")
+    end
+  },
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+  {
+    "MattesGroeger/vim-bookmarks"
+  },
+  {
+    "tom-anders/telescope-vim-bookmarks.nvim",
+    config = function()
+      require('telescope').load_extension('vim_bookmarks')
+    end
+  }
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
--- zshrc
+++ zshrc
@@ -1,22 +1,28 @@
-if type brew &>/dev/null
+if [[ "$OSTYPE" == "darwin" ]];
 then
-  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-  autoload -Uz compinit
-  compinit
+	if type brew &>/dev/null
+	then
+		FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+		autoload -Uz compinit
+		compinit
+	fi
 fi
 
 if [[ ! -d ~/.zplug ]]; then
-    git clone https://github.com/zplug/zplug ~/.zplug
-    source ~/.zplug/init.zsh && zplug update --self
+	git clone https://github.com/zplug/zplug ~/.zplug
+	source ~/.zplug/init.zsh && zplug update --self
 fi
 
 export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/.bin:$PATH"
-export PATH="$(brew --prefix)/opt/python/libexec/bin:$PATH"
-export PATH=/Users/dannyfritz/.local/share/ponyup/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
+if type brew &>/dev/null
+then
+	export PATH="$(brew --prefix)/opt/python/libexec/bin:$PATH"
+fi
 
 if [[ -d ~/.zplug ]]; then
-    source ~/.zplug/init.zsh
+	source ~/.zplug/init.zsh
 fi
 
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
@@ -28,9 +34,9 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 # zplug "MikeDacre/tmux-zsh-vim-titles", defer:2
 
 # Utilities
-zplug "zsh-users/zsh-history-substring-search", defer:2
-zplug "zsh-users/zsh-autosuggestions", defer:2
-zplug "hlissner/zsh-autopair", defer:2
+#zplug "zsh-users/zsh-history-substring-search", defer:2
+#zplug "zsh-users/zsh-autosuggestions", defer:2
+#zplug "hlissner/zsh-autopair", defer:2
 # zplug "MikeDacre/careful_rm", defer:2
 zplug "ael-code/zsh-colored-man-pages", defer:2
 
@@ -38,16 +44,16 @@ zplug "ael-code/zsh-colored-man-pages", defer:2
 zplug "kiurchv/asdf.plugin.zsh", defer:2
 zplug "plugins/git", from:oh-my-zsh, defer:2
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    zplug "plugins/debian", from:oh-my-zsh, defer:2
-    zplug "plugins/sudo", from:oh-my-zsh, defer:2
+	zplug "plugins/debian", from:oh-my-zsh, defer:2
+	zplug "plugins/sudo", from:oh-my-zsh, defer:2
 fi
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
+	printf "Install? [y/N]: "
+	if read -q; then
+		echo; zplug install
+	fi
 fi
 
 # Then, source plugins and add commands to $PATH
@@ -59,7 +65,6 @@ stty -ixon
 bindkey -v
 export KEYTIMEOUT=1
 
-
 ZSH_AUTOSUGGEST_USE_ASYNC="yes"
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=4"
 
@@ -69,7 +74,7 @@ AGKOZAK_BLANK_LINES=1
 AGKOZAK_CMD_EXEC_TIME=0
 
 ZSH_COMMAND_TIME_MIN_SECONDS=1
-ZSH_COMMAND_TIME_MSG="⏱: %s"
+ZSH_COMMAND_TIME_MSG="%s"
 ZSH_COMMAND_TIME_COLOR="cyan"
 
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
@@ -77,31 +82,30 @@ export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 export FZF_DEFAULT_COMMAND="rg --files"
 export FZF_DEFAULT_OPTS="--layout=reverse --inline-info --bind='enter:execute(xdg-open {})+clear-query'"
 
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+#bindkey '^[[A' history-substring-search-up
+#bindkey '^[[B' history-substring-search-down
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    alias ls='ls --color=auto'
+	alias ls='ls --color=auto'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    alias ls='ls -G'
-    alias vim=nvim
-    . /opt/homebrew/opt/asdf/libexec/asdf.sh
+	alias ls='ls -G'
+	alias vim=nvim
+	. /opt/homebrew/opt/asdf/libexec/asdf.sh
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-    alias ls='ls --color=auto'
+	alias ls='ls --color=auto'
 elif [[ "$OSTYPE" == "msys" ]]; then
-    alias ls='ls --color=auto'
+	alias ls='ls --color=auto'
 else
-    echo "What OS is this? $OSTYPE"
+	echo "What OS is this? $OSTYPE"
 fi
 
 export TERMINFO_DIRS=$TERMINFO_DIRS:$HOME/.local/share/terminfo
 
 #$SHLVL != "2" && (tmux attach || tmux new)
 if [[ "$SHLVL" -lt 2 ]]; then
-    tmux
+	tmux
 fi
-
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
