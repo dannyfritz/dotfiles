acce3b16d3ef52246156a1b1362306548f98910d
Danny Fritz <me@dannyfritz.com>
2023-09-24 19:10:46 -0500

    nvim config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,6 +15,8 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "plugins" },
   },
   defaults = {
--- nvim/lua/plugins/file-tree.lua
+++ nvim/lua/plugins/file-tree.lua
@@ -2,9 +2,19 @@ return {
   { "nvim-tree/nvim-web-devicons", enabled = false },
   {
     "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
     opts = {
       window = {
         position = "right",
+      },
+    },
+  },
+  {
+    "echasnovski/mini.files",
+    opts = {
+      options = {
+        permanent_delete = false,
+        use_as_default_explorer = true,
       },
     },
   },
