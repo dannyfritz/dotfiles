19c0cf987f90ede44323fea368944a317994b24d
Danny Fritz <me@dannyfritz.com>
2023-09-30 15:59:48 -0500

    restore completions. remove terminal keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader><tab>z", "<cmd>tabedit %<cr>", { desc = "Zoom to Tab" })
+vim.keymap.set("n", "<Leader>ft", "<Nop>")
+vim.keymap.set("n", "<Leader>fT", "<Nop>")
--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,10 +1,17 @@
 return {
-  { "L3MON4D3/LuaSnip", enabled = false },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = cmp.mapping.preset.insert()
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function(_, opts)
+  --     local cmp = require("cmp")
+  --     opts.mapping = cmp.mapping.preset.insert({
+  --       ["<Tab>"] = {
+  --         -- cmp.confirm({ select = false }),
+  --       },
+  --     })
+  --   end,
+  -- },
 }
