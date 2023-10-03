cfb48b3ec6fd1defbcf62b909d8a36315390536e
Danny Fritz <me@dannyfritz.com>
2023-10-02 20:20:32 -0500

    setup cleanup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,28 @@
-ln -sf $(pwd)/alacritty.yml ~/.alacritty.yml
+# Alacritty
+mkdir -p ~/.config/alacritty
+ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
+# asdf
 ln -sf $(pwd)/asdfrc ~/.asdfrc
-ln -sf $(pwd)/gitconfig ~/.gitconfig
-ln -sf $(pwd)/gitignore ~/.gitignore
-mkdir -p ~/.config/tmux
-ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
-ln -sf $(pwd)/zshrc ~/.zshrc
-mkdir -p ~/.config/kitty
-ln -sf $(pwd)/kitty.conf  ~/.config/kitty/kitty.conf
+# git
+mkdir -p ~/.config/git
+ln -sf $(pwd)/gitconfig ~/.config/git/config
+ln -sf $(pwd)/gitignore ~/.config/git/ignore
+# fish
 mkdir -p ~/.config/fish
 ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
 ln -sf $(pwd)/fish_plugins ~/.config/fish/fish_plugins
+# gitui
 mkdir -p ~/.config/gitui
 ln -sf $(pwd)/gitui.ron ~/.config/gitui/key_bindings.ron
-ln -sf $(pwd)/starship.toml ~/.config/starship.toml
+# kitty
+mkdir -p ~/.config/kitty
+ln -sf $(pwd)/kitty.conf  ~/.config/kitty/kitty.conf
+# nvim
 ln -sf $(pwd)/nvim ~/.config/nvim
+# starship
+ln -sf $(pwd)/starship.toml ~/.config/starship.toml
+# tmux
+mkdir -p ~/.config/tmux
+ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
+# zsh
+ln -sf $(pwd)/zshrc ~/.zshrc
