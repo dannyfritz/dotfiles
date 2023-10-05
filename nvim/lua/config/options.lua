24516c828ffd80c979b3f9006911e7c03ad6a572
Danny Fritz <me@dannyfritz.com>
2023-10-04 22:40:44 -0500

    lazyvim update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.conceallevel = 0
-vim.opt.scrolloff = 20
+vim.opt.scrolloff = 30
 vim.opt.mouse = ""
 vim.cmd("highlight ColorColumn guibg=#151820")
 vim.opt.colorcolumn = { 80, 100 }
--- nvim/lua/plugins/animate.lua
+++ nvim/lua/plugins/animate.lua
@@ -1,17 +1,25 @@
 return {
   "echasnovski/mini.animate",
-  opts = {
-    cursor = {
-      enable = false,
-    },
-    resize = {
-      enable = false,
-    },
-    open = {
-      enable = false,
-    },
-    close = {
-      enable = false,
+  opts = function()
+    local animate = require("mini.animate")
+    return {
+      cursor = {
+        enable = false,
+      },
+      resize = {
+        enable = false,
+      },
+      open = {
+        enable = false,
+      },
+      close = {
+        enable = false,
+      },
+      scroll = {
+        enable = true,
+        timing = animate.gen_timing.linear({ easint = "out", duration = 8 }),
+        subscroll = animate.gen_subscroll.equal({ max_output_steps = 6 }),
+      },
     }
-  }
+  end,
 }
