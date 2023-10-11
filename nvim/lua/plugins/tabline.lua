23c2349fe494d7a52691d09a6ac4c06117d2f3e7
Danny Fritz <me@dannyfritz.com>
2023-10-10 19:52:44 -0500

    bufferline option with no change

--- nvim/lua/plugins/tabline.lua
+++ nvim/lua/plugins/tabline.lua
@@ -3,6 +3,7 @@ return {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
+        always_show_bufferline = false,
         show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
