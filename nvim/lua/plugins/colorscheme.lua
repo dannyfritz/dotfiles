9e6a8d9031a94cac9391f1fe00ab98277d664dd9
Danny Fritz <me@dannyfritz.com>
2023-10-14 20:13:17 -0500

    update lazyvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,24 +2,12 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
   {
-    "mcchrish/zenbones.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    init = function()
-      vim.g.neobones_darkness = "stark"
-      vim.g.neobones_lighten_cursor_line = 8
-      vim.g.neobones_lighten_comments = 40
-      vim.g.neobones_italic_comments = false
-      vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
-        callback = function()
-          vim.cmd("highlight ColorColumn guibg=#151820")
-        end,
-      })
-    end,
+    "rktjmp/lush.nvim"
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "neobones",
+      colorscheme = "bleak",
     },
-  },
+  }
 }
