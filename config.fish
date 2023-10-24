84f0a4426c18e8a2f0c1424f9d28838cf179f1e4
Danny Fritz <me@dannyfritz.com>
2023-10-24 09:31:06 -0500

    warn on programs that are expected, but missing

--- config.fish
+++ config.fish
@@ -15,11 +15,46 @@ if test -z $TMUX
     exit 0
 end
 
-# https://starship.rs/
+set --erase expected_programs
+set expected_programs $expected_programs asdf\;https://yazi-rs.github.io/
+set expected_programs $expected_programs bat\;https://github.com/sharkdp/bat
+set expected_programs $expected_programs entr\;https://github.com/eradman/entr
+set expected_programs $expected_programs fzf\;https://github.com/junegunn/fzf
+set expected_programs $expected_programs glow\;https://github.com/charmbracelet/glow
+set expected_programs $expected_programs htop\;https://htop.dev/
+set expected_programs $expected_programs jq\;https://github.com/jqlang/jq
+set expected_programs $expected_programs lsd\;https://github.com/lsd-rs/lsd
+set expected_programs $expected_programs nvim\;https://neovim.io/
+set expected_programs $expected_programs rg\;https://github.com/BurntSushi/ripgrep
+set expected_programs $expected_programs starship\;https://starship.rs/
+set expected_programs $expected_programs yazi\;https://yazi-rs.github.io/
+for expected_program in $expected_programs
+  begin
+    string match -q -r '(?<program>.*);(?<website>.*)' $expected_program
+    if not type -q $program
+      set_color normal 
+      set_color bryellow
+      echo -n Warning:\ 
+      set_color -o brgreen
+      echo -n $program\ 
+      set_color normal 
+      echo is not installed or is missing from the \$PATH!
+      if test -n $website
+        set_color normal 
+        echo -n "        " Visit\ 
+        set_color -o brwhite
+        echo -n $website
+        set_color normal 
+        echo \ for more information.
+        set_color brwhite
+      end
+      set_color normal 
+    end
+  end
+end
+
 if type -q starship
-    starship init fish | source
-else
-    echo "Error: Prompt missing. Please install Starship https://starship.rs/"
+  starship init fish | source
 end
 
 # https://fishshell.com/docs/current/interactive.html#vi-mode-commands
