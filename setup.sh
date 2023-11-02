8d294f17964f13cc4c08154d95b5f883531e6357
Danny Fritz <me@dannyfritz.com>
2023-11-01 21:07:49 -0500

    lsd config

--- setup.sh
+++ setup.sh
@@ -1,30 +1,33 @@
 # Alacritty
 mkdir -p ~/.config/alacritty
-ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -hwsf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 # asdf
-ln -sf $(pwd)/asdfrc ~/.asdfrc
+ln -hwsf $(pwd)/asdfrc ~/.asdfrc
 # git
 mkdir -p ~/.config/git
-ln -sf $(pwd)/gitconfig ~/.config/git/config
-ln -sf $(pwd)/gitignore ~/.config/git/ignore
+ln -hwsf $(pwd)/gitconfig ~/.config/git/config
+ln -hwsf $(pwd)/gitignore ~/.config/git/ignore
 # fish
 mkdir -p ~/.config/fish
-ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
-ln -sf $(pwd)/fish_plugins ~/.config/fish/fish_plugins
+ln -hwsf $(pwd)/config.fish ~/.config/fish/config.fish
+ln -hwsf $(pwd)/fish_plugins ~/.config/fish/fish_plugins
 # gitui
 mkdir -p ~/.config/gitui
-ln -sf $(pwd)/gitui.ron ~/.config/gitui/key_bindings.ron
+ln -hwsf $(pwd)/gitui.ron ~/.config/gitui/key_bindings.ron
 # kitty
 mkdir -p ~/.config/kitty
-ln -sf $(pwd)/kitty.conf  ~/.config/kitty/kitty.conf
+ln -hwsf $(pwd)/kitty.conf  ~/.config/kitty/kitty.conf
+# lsd
+mkdir -p ~/.config/lsd
+ln -hwsf $(pwd)/lsd.yaml ~/.config/lsd/config.yaml
 # nvim
-ln -sf $(pwd)/nvim ~/.config/nvim
+ln -hwsf $(pwd)/nvim ~/.config/nvim
 # starship
-ln -sf $(pwd)/starship.toml ~/.config/starship.toml
+ln -hwsf $(pwd)/starship.toml ~/.config/starship.toml
 # tmux
 mkdir -p ~/.config/tmux
-ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
+ln -hwsf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
 # yazi
-ln -sf $(pwd)/yazi ~/.config/yazi
+ln -hwsf $(pwd)/yazi ~/.config/yazi
 # zsh
-ln -sf $(pwd)/zshrc ~/.zshrc
+ln -hwsf $(pwd)/zshrc ~/.zshrc
