60f6735f21cee176249b1fee4c32f3f33af52339
Danny Fritz <me@dannyfritz.com>
2024-02-24 14:50:10 -0600

    more expected programs

--- config.fish
+++ config.fish
@@ -24,7 +24,9 @@ if status --is-interactive
   set -a expected_programs entr\;https://github.com/eradman/entr
   set -a expected_programs fd\;https://github.com/sharkdp/fd
   set -a expected_programs figlet\;http://www.figlet.org/
+  # set -a expected_programs fish\;https://fishshell.com/
   set -a expected_programs fzf\;https://github.com/junegunn/fzf
+  set -a expected_programs gitui\;https://github.com/extrawurst/gitui
   set -a expected_programs glow\;https://github.com/charmbracelet/glow
   set -a expected_programs htop\;https://htop.dev/
   set -a expected_programs jq\;https://github.com/jqlang/jq
@@ -35,6 +37,7 @@ if status --is-interactive
   set -a expected_programs rg\;https://github.com/BurntSushi/ripgrep
   set -a expected_programs sox\;https://sox.sourceforge.net/
   set -a expected_programs starship\;https://starship.rs/
+  set -a expected_programs tmux\;https://github.com/tmux/tmux
   set -a expected_programs utimer\;https://launchpad.net/utimer/
   set -a expected_programs yazi\;https://yazi-rs.github.io/
   for expected_program in $expected_programs
