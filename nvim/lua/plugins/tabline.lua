2ecf9299d4c8708ddf727adca487a880e4ccfdb0
Danny Fritz <me@dannyfritz.com>
2024-03-04 09:56:41 -0600

    nvim tabline

--- nvim/lua/plugins/tabline.lua
+++ nvim/lua/plugins/tabline.lua
@@ -3,7 +3,7 @@ return {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
-        always_show_bufferline = false,
+        always_show_bufferline = true,
         show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
@@ -11,4 +11,24 @@ return {
       },
     },
   },
+  -- {
+  --   "b0o/incline.nvim",
+  --   opts = {
+  --     render = function(props)
+  --       local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+  --       local modified = vim.bo[props.buf].modified
+  --       if props.focused == false then
+  --         return nil
+  --       end
+  --       return {
+  --         " ",
+  --         filename,
+  --         modified and { " *", guifg = "#888888", gui = "bold" } or "",
+  --         " ",
+  --         guibg = "#111111",
+  --         guifg = "#eeeeee",
+  --       }
+  --     end,
+  --   },
+  -- },
 }
