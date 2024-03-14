7e666e2011acb70b70a2e6ae3f968f2d74add21e
Danny Fritz <me@dannyfritz.com>
2024-03-14 16:22:55 -0500

    astro: tabline, statusbar, winbar

--- astronvim/plugins/heirline.lua
+++ astronvim/plugins/heirline.lua
@@ -3,19 +3,6 @@ return {
 	opts = function(_, opts)
 		local status = require("astronvim.utils.status")
 		local get_hlgroup = require("astronvim.utils").get_hlgroup
-		local status_hl = get_hlgroup("StatusLine")
-		local hl = { fg = status_hl.fg ,bg = status_hl.bg }
-		opts.statusline = {
-			status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
-			status.component.nav({ hl = hl, scrollbar = false, ruler = {}, percentage = {}, padding = { right = 1 } }),
-			status.component.file_info { hl = hl, filetype = false, filename = {}, file_modified = false },
-			status.component.diagnostics(),
-			-- status.component.git_branch(),
-			status.component.fill(),
-			-- status.component.lsp(),
-			-- status.component.treesitter(),
-			status.component.mode({ surround = { separator = "right" } }),
-		}
 		opts.tabline[2] = status.heirline.make_buflist({
 			{
 				provider = " ",
@@ -38,6 +25,43 @@ return {
 				close_button = false,
 			}),
 		})
+		opts.winbar = {
+			init = function(self)
+				self.bufnr = vim.api.nvim_get_current_buf()
+			end,
+			fallthrough = false,
+			{
+				status.component.separated_path({
+					path_func = status.provider.filename({ modify = ":.:h" }),
+					separator = "/",
+				}),
+				status.component.file_info({
+					file_icon = false,
+					file_modified = false,
+					file_read_only = false,
+					surround = false,
+					update = "BufEnter",
+				}),
+			},
+		}
+		local status_hl = get_hlgroup("StatusLine")
+		opts.statusline = {
+			status.component.mode({ mode_text = { padding = { left = 1, right = 1 } } }),
+			status.component.nav({
+				hl = status_hl,
+				scrollbar = false,
+				ruler = {},
+				percentage = {},
+				padding = { right = 1 },
+			}),
+			status.component.diagnostics(),
+			-- status.component.file_info({ hl = status_hl, filetype = false, filename = {}, file_modified = false }),
+			-- status.component.git_branch(),
+			status.component.fill(),
+			-- status.component.lsp(),
+			-- status.component.treesitter(),
+			status.component.mode({ surround = { separator = "right" } }),
+		}
 		return opts
 	end,
 }
