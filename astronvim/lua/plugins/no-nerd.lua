a933a2c188d01bb8f69f87d110478bb53f4bc6f2
Danny Fritz <me@dannyfritz.com>
2024-03-19 22:03:02 -0500

    astro: more removing nerd

--- astronvim/lua/plugins/no-nerd.lua
+++ astronvim/lua/plugins/no-nerd.lua
@@ -1,4 +1,10 @@
 return {
   { "folke/todo-comments.nvim", opts = { signs = false } },
-  { "folke/which-key.nvim", opts = { icons = { separator = "|" } } },
+  { "folke/which-key.nvim",
+    opts = function(_, opts)
+      local get_icon = require("astroui").get_icon
+      opts.icons.separator = get_icon("ArrowRight")
+      return opts
+    end,
+  }
 }
