eb361f3a5aa21b1567c3e52307d6f178d3f1ae10
Danny Fritz <me@dannyfritz.com>
2024-09-24 13:38:46 -0500

    fish: require btop instead of htop

--- fish/expected_programs.fish
+++ fish/expected_programs.fish
@@ -6,17 +6,16 @@ if status --is-interactive
     set --erase expected_programs
     set -a expected_programs asdf\;https://asdf-vm.com
     set -a expected_programs bat\;https://github.com/sharkdp/bat
-    # set -a expected_programs cowsay\;https://github.com/tnalpgge/rank-amateur-cowsay
     set -a expected_programs delta\;https://github.com/dandavison/delta
     set -a expected_programs entr\;https://github.com/eradman/entr
     set -a expected_programs fd\;https://github.com/sharkdp/fd
     set -a expected_programs figlet\;http://www.figlet.org
-    # set -a expected_programs fish\;https://fishshell.com/
-    # set -a expected_programs fortune\;http://bxr.su/OpenBSD/games/fortune
+    set -a expected_programs fish\;https://fishshell.com/ # silly check within fish but whatever
+    set -a expected_programs fortune\;http://bxr.su/OpenBSD/games/fortune
     set -a expected_programs fzf\;https://github.com/junegunn/fzf
     set -a expected_programs gitui\;https://github.com/extrawurst/gitui
     set -a expected_programs glow\;https://github.com/charmbracelet/glow
-    set -a expected_programs htop\;https://htop.dev
+    set -a expected_programs btop\;https://github.com/aristocratos/btop
     set -a expected_programs jq\;https://github.com/jqlang/jq
     set -a expected_programs lsd\;https://github.com/lsd-rs/lsd
     set -a expected_programs noti\;https://github.com/variadico/noti
