7f3735f95511cf6d89e13374d485211c8af3fdba
Danny Fritz <me@dannyfritz.com>
2024-09-27 15:44:40 -0500

    fish: new pomodoro timer

--- fish/expected_programs.fish
+++ fish/expected_programs.fish
@@ -10,7 +10,7 @@ if status --is-interactive
     set -l -a expected_programs fortune\;http://bxr.su/OpenBSD/games/fortune
     set -l -a expected_programs fzf\;https://github.com/junegunn/fzf
     set -l -a expected_programs gitui\;https://github.com/extrawurst/gitui
-    set -l -a expected_programs glow\;https://github.com/charmbracelet/glow
+    # set -l -a expected_programs glow\;https://github.com/charmbracelet/glow
     set -l -a expected_programs jq\;https://github.com/jqlang/jq
     set -l -a expected_programs lsd\;https://github.com/lsd-rs/lsd
     set -l -a expected_programs noti\;https://github.com/variadico/noti
@@ -18,8 +18,9 @@ if status --is-interactive
     set -l -a expected_programs rg\;https://github.com/BurntSushi/ripgrep
     set -l -a expected_programs sox\;https://sox.sourceforge.net
     set -l -a expected_programs starship\;https://starship.rs
+    set -l -a expected_programs timer\;https://github.com/caarlos0/timer
     set -l -a expected_programs tmux\;https://github.com/tmux/tmux
-    set -l -a expected_programs utimer\;https://launchpad.net/utimer
+    # set -l -a expected_programs utimer\;https://launchpad.net/utimer
     set -l -a expected_programs yazi\;https://yazi-rs.github.io
     for expected_program in $expected_programs
         begin
--- fish/pomodoro.fish
+++ fish/pomodoro.fish
@@ -1,6 +1,51 @@
-if type -q figlet; and type -q utimer; and type -q noti; and type -q play
-    abbr --add pmdsb "figlet \"Short break...\"; and utimer --countdown=6m; and noti -t \"Pomodoro\" -m \"Time to focus: pmdf\"; and play -q ~/.config/sounds/birds-chirping-in-a-forest.mp3 silence 1 5 2% trim 0 10 &;"
-    abbr --add pmdlb "figlet \"Long break...\"; and utimer --countdown=12m; and noti -t \"Pomodoro\" -m \"Time to focus: pmdf\"; and play -q ~/.config/sounds/birds-chirping-in-a-forest.mp3 silence 1 5 2% trim 0 10 &;"
-    abbr --add pmdf "figlet \"Focus...\"; and utimer --countdown=20m; and noti -t \"Pomodoro\" -m \"Time for a break: pmdsb pmdlb\"; and play -q ~/.config/sounds/birds-chirping-in-a-forest.mp3 silence 1 5 2% trim 0 10 &;"
+function pomodoro
+    if ! type -q jq;
+        echo '\"jq\" is required to use Pomodoro'
+        return 1
+    end
+    set -f COMPLETE_SOUND ~/.config/sounds/birds-chirping-in-a-forest.mp3
+    set -f -a OPTIONS (echo '{"focus": {"text": "Focus...", "time": "24m"}, "rest": { "text": "Short Break...", "time": "12m" }, "break": {"text": "Stand Up...", "time": "6m"}}' | jq -c)
+    set -f OPTION (echo $OPTIONS | jq -c -r (printf '.["%s"]' $argv))
+    if test $OPTION = "null";
+        set_color yellow
+        printf 'Unknown OPTION: "%s"\n\n' $argv
+        set_color -o brwhite
+        printf "USAGE:\n"
+        set_color normal
+        printf "\tpomodoro OPTION\n\n"
+        set_color -o brwhite
+        printf "ARGUMENTS:\n"
+        set_color normal
+        printf "\tOPTION: %s\n" (echo $OPTIONS | jq -c 'keys')
+        set_color normal
+        return 1
+    end
+    set -f TEXT (echo $OPTION | jq -r '.text')
+    set -f TIME (echo $OPTION | jq -r '.time')
+    command figlet $TEXT
+    if type -q timer;
+        command timer -f --format 24h $TIME
+    else if type -q utimer;
+        command utimer --countdown=$TIME
+    else
+        echo 'Missing "timer" or "utimer" commands.'
+        return 1
+    end
+    if test $status -ne 0;
+        echo "Session ended early"
+        return 1
+    end
+    if type -q noti;
+        command noti -t "Pomodoro" -m (printf "%s session is done." $argv)
+    end
+    if type -q play;
+        and test -e "$COMPLETE_SOUND"
+        command play -q $COMPLETE_SOUND silence 1 5 2% trim 0 10 &;
+    else if type -q say;
+        command say (printf "%s session is done." $argv)
+    else if type -q spd-say;
+        command spd-say (printf "%s session is done." $argv)
+    end
+    return 0
 end
 
