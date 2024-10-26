a1e7e53d76632d64cc0233b9b27d891d3f39ac3a
Danny Fritz <me@dannyfritz.com>
2024-10-25 22:12:42 -0500

    fish: add rgz-fzf

--- fish/config.fish
+++ fish/config.fish
@@ -77,6 +77,26 @@ if type -q fzf
     end
 end
 
+if type -q fzf;
+    and type -q rga
+    function rga-fzf
+        set RG_PREFIX 'rga --files-with-matches'
+        if test (count $argv) -gt 1
+            set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
+        end
+        set -l file $file
+        set file (
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
+        fzf --sort \
+            --preview='test ! -z {} && \
+                rga --pretty --context 5 {q} {}' \
+            --phony -q "$argv[-1]" \
+            --bind "change:reload:$RG_PREFIX {q}" \
+            --preview-window='50%:wrap'
+    ) && echo "opening $file" && open "$file"
+    end
+end
+
 function dotfiles -d "Modify and sync dotfiles"
     set -f LOCATION ~/dotfiles
     function __dotfileshelp -V LOCATION
